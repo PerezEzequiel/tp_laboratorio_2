6c43e876350375ce37894ef32c22bffb7abe0fd9
Ezequiel Perez <ezequiel_agustin12@hotmail.com>
2021-10-03 22:35:36 -0300

    TP NRO 2

---
+++ TP-02/Entidades/Suv.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Text;
+
+namespace Entidades
+{
+    public class Suv : Vehiculo
+    {
+        #region Constructor 
+        public Suv(EMarca marca, string chasis, ConsoleColor color)
+        : base(chasis, marca, color)
+        {
+
+        }
+        #endregion
+
+        #region Propiedad
+        /// <summary>
+        /// SUV son 'Grande'
+        /// </summary>
+        protected override ETamanio Tamanio
+        {
+            get
+            {
+                return ETamanio.Grande;
+            }
+        }
+
+        #endregion
+
+        #region Metodo
+        /// <summary>
+        /// Devuelve las caracteristicas del Suv.
+        /// </summary>
+        /// <returns></returns>
+        public override string Mostrar()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine("SUV");
+            sb.AppendLine(base.Mostrar());
+            sb.Append($"TAMAÑO : {this.Tamanio}");
+            sb.AppendLine("");
+            sb.AppendLine("---------------------");
+
+            return sb.ToString();
+        }
+        #endregion
+    }
+}
---
+++ TP-02/Entidades/Vehiculo.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Text;
+
+namespace Entidades
+{
+    /// <summary>
+    /// La clase Vehiculo no deberá permitir que se instancien elementos de este tipo.
+    /// </summary>
+    public abstract class Vehiculo
+    {
+        #region Enumerados
+        public enum EMarca
+        {
+            Chevrolet, Ford, Renault, Toyota, BMW, Honda, HarleyDavidson
+        }
+        public enum ETamanio
+        {
+            Chico, Mediano, Grande
+        }
+
+        #endregion
+        
+        #region Atributos
+        private EMarca marca;
+        private string chasis;
+        private ConsoleColor color;
+        #endregion
+
+        #region Propiedad
+        /// <summary>
+        /// ReadOnly: Retornará el tamaño
+        /// </summary>
+        protected abstract ETamanio Tamanio
+        {
+            get;
+        }
+        #endregion
+
+        #region Constructor
+
+        public Vehiculo(string chasis, EMarca marca,ConsoleColor color)
+        {
+            this.chasis = chasis;
+            this.marca = marca;
+            this.color = color;
+        }
+
+        #endregion
+
+        #region Metodo
+        /// <summary>
+        /// Publica todos los datos del Vehiculo.
+        /// </summary>
+        /// <returns></returns>
+        public virtual string Mostrar()
+        {
+            return (string)this;
+        }
+        #endregion
+
+        #region Sobrecargas
+
+        /// <summary>
+        /// Formatea y devuelve los datos de un vehiculo.
+        /// </summary>
+        /// <param name="p">Vehiculo recibido</param>
+        public static explicit operator string(Vehiculo p)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.Append($"CHASIS: {p.chasis}\r\n");
+            sb.Append($"MARCA : {p.marca}\r\n");
+            sb.Append($"COLOR : {p.color}\r\n");
+            sb.AppendLine("---------------------");
+
+            return sb.ToString();
+        }
+
+        /// <summary>
+        /// Dos vehiculos son iguales si comparten el mismo chasis
+        /// </summary>
+        /// <param name="v1"></param>
+        /// <param name="v2"></param>
+        /// <returns></returns>
+        public static bool operator ==(Vehiculo v1, Vehiculo v2)
+        {
+            return v1.chasis == v2.chasis;
+        }
+        /// <summary>
+        /// Dos vehiculos son distintos si su chasis es distinto
+        /// </summary>
+        /// <param name="v1"></param>
+        /// <param name="v2"></param>
+        /// <returns></returns>
+        public static bool operator !=(Vehiculo v1, Vehiculo v2)
+        {
+            return !(v1 == v2);
+        }
+        #endregion
+    }
+}
